0cd8cf64a3941983d183d2b4cbddb2a45058822c
Manjushri Bhosale <manjushribhosale@gmail.com>
2019-03-04 14:19:24 -0500

    Added configuration for CORS
    Added configuration for CORS

---
+++ src/main/java/com/ford/turbo/DefaultApiConfig.java
@@ -0,0 +1,23 @@
+package com.ford.turbo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class DefaultApiConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(final CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+    }
+}
